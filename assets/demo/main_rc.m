ec5cef0b16093ae9b0fadac1b7773681ea750e57
Nidish Narayanaa Balaji <nidbid@gmail.com>
2024-01-29 07:06:04 -0500

    top

--- assets/demo/main_rc.m
+++ assets/demo/main_rc.m
@@ -6,20 +6,20 @@ set(0,'defaultTextInterpreter','latex');
 set(0, 'DefaultLegendInterpreter', 'latex');
 set(0,'defaultAxesFontSize',13);
 
-%% Read Nodes and Elements
+%% Read Nodes and Elements
 Nodes = dlmread('./Nodes.dat');
 Elements = dlmread('./Elements.dat');
 N = size(Nodes,1);  % Number of nodes per side
 Ne = size(Elements,1);  % Number of elements
 
-%% Load Matrices
+%% Load Matrices
 fname = './Modelmats.mat';
 load(fname, 'M', 'K', 'R', 'Fv');
 
 % Number of generalized modal DOFs
 Nint = size(M,1)-N*3;
 
-%% Single-Point-Quadrature Matrices
+%% Single-Point-Quadrature Matrices
 Qm = zeros(Ne, N);  % Each element has one quadrature point
 Tm = zeros(N, Ne);  % Each quadrature pt integrated to 4 nodes
 Ars = zeros(Ne, 1);  % Vector of areas of each element
@@ -40,21 +40,21 @@ Ar_tot = 120e-3*25.4e-3-3*pi*(0.85e-2/2)^2; % True total area
                                             % Rectangle-3*Circles
 Ar_avg = Ar_tot/Ne;  % Average element area
 
-%% Contact Relative Displacements (DOFS already in relative coordinates
+%% Contact Relative Displacements (DOFS already in relative coordinates
 Lz = [kron(Qm, [0 0 1]) zeros(Ne, Nint)]; % Get only normal displacement
 Gz = [kron(Tm, [0; 0; 1]); zeros(Nint, Ne)];
 
 % Nodal relative disp (only for plotting)
 Lz_n = [kron(eye(N), [0 0 1]) zeros(N, Nint)]; % Get only normal displacement
 
-%% Remove fixed interface null-space
+%% Remove fixed interface null-space
 L1 = null(Lz);
 [V,D] = eigs(L1'*K*L1, L1'*M*L1, 20, 'SM'); % Get first 20 modes
 Ln = null(V(:, 1:6)'*L1'*M);  % First six modes are RBMs
 
 Nn = size(Ln, 2);  % Null-reduced DOFs
 
-%% Solve the Nonlinear Static Prestress Problem
+%% Solve the Nonlinear Static Prestress Problem
 bpmag = 12e3;
 knl = 5e6/(Ar_tot/Ne); % knl divided by avg element area
 U0 = (Ln'*K*Ln + (Gz'*Ln)'*(Lz*Ln)*knl)\(Ln'*Fv*bpmag);
@@ -62,12 +62,12 @@ U0 = (Ln'*K*Ln + (Gz'*Ln)'*(Lz*Ln)*knl)\(Ln'*Fv*bpmag);
 opt = optimoptions('fsolve', 'specifyObjectiveGradient', true, 'Display', 'iter');
 [U0, ~, ~, ~, J0] = fsolve(@(U) RESFUN([U; bpmag], Ln'*K*Ln, Ln'*Fv, Lz*Ln, Ln'*Gz, knl), U0, opt);
 
-%% Linearized Modal Analysis
+%% Linearized Modal Analysis
 [V0, D0] = eigs(J0, Ln'*M*Ln, 10, 'SM');
 W0 = sqrt(diag(D0));
 disp(table((1:10)', W0/2/pi, 'VariableNames', {'Index', 'Frequency (Hz)'}))
 
-%% Depict Interfacial Displacement Field
+%% Depict Interfacial Displacement Field
 A = zeros(N);  % Graph adjacency matrix
 for ei=1:Ne
        ndis = Elements(ei, 2:end);  % Nodes of the current element
@@ -97,7 +97,7 @@ xlabel(xx, 'Nodal Relative Displacement (m)', 'interpreter', 'latex', 'fontsize'
 
 print('./intdisps_rc.png', '-dpng', '-r300')
 
-%% Show Quadrature Nodes
+%% Show Quadrature Nodes
 figure(2)
 pos=get(gcf, 'Position');
 set(gcf, 'Position', [pos(1:2) 900 260], 'Color', 'white')
@@ -116,7 +116,7 @@ ylabel('Y Coordinate (m)')
 
 print('./quadpts_rc.png', '-dpng', '-r300')
 
-%% Show Pressure field
+%% Show Pressure field
 pvals = max(knl*Lz*Ln*U0, 0);  % pressure @ QPs
 figure(3)
 pos=get(gcf, 'Position');
